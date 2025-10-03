d112d3c6cc369b12a5bd6b6d2cfc734e321db204
阿久津匠輝 <shoki.akutsu.vantan@gmail.com>
2025-10-03 18:36:52 +0900

    [Update] 1P用にタイトル画面を変えた

--- Assets/Title/Script/InGameSceneManager.cs
+++ Assets/Title/Script/InGameSceneManager.cs
@@ -5,29 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class InGameSceneManager : MonoBehaviour
 {
-	GlowEffect[] _glow;
-	bool _isNext;
 	int _inGameIndex = 1;
-
-	private void Start()
-	{
-		_glow = GameObject.FindObjectsOfType<GlowEffect>();
-	}
 
 	void Update()
     {
-		_isNext = true;
-
-        foreach (GlowEffect g in _glow)
-		{
-			if(!g.Active)
-			{
-				_isNext = false;
-				break;
-			}
-		}
-
-		if (_isNext)
+		if (Input.GetKeyDown(KeyCode.Return))
 		{
 			SceneManager.LoadScene(_inGameIndex);
 		}
